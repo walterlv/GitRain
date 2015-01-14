88d742af3f845c738746fc7057d20fe5e76dbbb8
walterlv <xblero@gmail.com>
2015-01-14 22:04:45 +0800

    replace the message frame usercontrol to customcontrol

---
+++ Code/GitRain.Program/UI/MessageFrame.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Cvte.GitRain.UI
+{
+    public class MessageFrame : ContentControl
+    {
+        static MessageFrame()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MessageFrame),
+                new FrameworkPropertyMetadata(typeof(MessageFrame)));
+        }
+    }
+}
