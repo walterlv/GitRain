c8e80d794118592ce93aa632badab050acce2b25
walterlv <xblero@gmail.com>
2015-01-15 10:32:00 +0800

    Add repo home page
    This is only a test page

--- Code/GitRain.Program/UI/GitRepoPanel.xaml.cs
+++ Code/GitRain.Program/UI/GitRepoPanel.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace Cvte.GitRain.UI
 {
@@ -7,6 +9,15 @@ namespace Cvte.GitRain.UI
         public GitRepoPanel()
         {
             InitializeComponent();
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            string pageTypeName = String.Format("{0}.Repo{1}Page",
+                GetType().Namespace, (string) (((FrameworkElement) e.Source).Tag));
+            Type pageType = Type.GetType(pageTypeName);
+            Page page = (Page)Activator.CreateInstance(pageType);
+            Frame.Content = page;
         }
     }
 }
